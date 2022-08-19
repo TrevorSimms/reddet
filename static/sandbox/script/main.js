7f14ecd0736181b95856bb5ecf827bc946f067f1
trevor-simms <trevor-simms@outlook.com>
2022-08-18 22:51:25 -0400

    Polished UI Functionality

--- static/sandbox/script/main.js
+++ static/sandbox/script/main.js
@@ -1,6 +1,14 @@
-// any scripts to be run after <body>
+/**
+ *  Author: Trevor Simms
+ *  Email: trevor-simms@outlook.com
+ *
+ *  Description:
+ *      Main JavaScript file for Reddet.
+ */
+// script variables
 var myDate = new Date();
 var footerDate = document.getElementById('footerDate');
+// simple date string
 if (footerDate != null) {
     footerDate.innerHTML = myDate.toTimeString();
 }
--- static/sandbox/script/main.ts
+++ static/sandbox/script/main.ts
@@ -1,7 +1,15 @@
-// any scripts to be run after <body>
+/**
+ *  Author: Trevor Simms 
+ *  Email: trevor-simms@outlook.com
+ * 
+ *  Description:
+ *      Main JavaScript file for Reddet.
+ */
+// script variables
 let myDate = new Date();
 let footerDate = document.getElementById('footerDate');
 
+// simple date string
 if(footerDate != null) {
     footerDate.innerHTML = myDate.toTimeString();
 }
